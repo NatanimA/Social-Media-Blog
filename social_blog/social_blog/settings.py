2cbb9c00ec7af8cffb96bf8e2ca668cc76afa747
NatanimA <66979348+NatanimA@users.noreply.github.com>
2022-06-30 19:36:38 +0300

    second

--- social_blog/social_blog/settings.py
+++ social_blog/social_blog/settings.py
@@ -22,7 +22,7 @@ TEMPLATE_DIR= os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e6n#(cosd*2d2qb@51#xabvpg#%$1wnxg1!9=69hx(pn_m@g0='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
